4ae899b3796199958c2db09cee66464d4899e81c
Sayantan Maity <sayantan.official52@gmail.com>
2025-08-25 19:05:35 +0530

    Update 01_table.sql

--- data/01_table.sql
+++ data/01_table.sql
@@ -1,1 +1,16 @@
+CREATE TABLE sales (
+  customer_id VARCHAR(1),
+  order_date DATE,
+  product_id INTEGER
+);
 
+CREATE TABLE menu (
+  product_id INTEGER,
+  product_name VARCHAR(20),
+  price INTEGER
+);
+
+CREATE TABLE members (
+  customer_id VARCHAR(1),
+  join_date DATE
+);
