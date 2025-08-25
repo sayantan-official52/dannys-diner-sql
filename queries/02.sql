01ea06b52c584f05c96594423a03db26ff2ab273
Sayantan Maity <sayantan.official52@gmail.com>
2025-08-25 19:16:18 +0530

    Update 02.sql

--- queries/02.sql
+++ queries/02.sql
@@ -1,4 +1,4 @@
---2.How many days has each customer visited the restaurant?
+-- 2.How many days has each customer visited the restaurant?
 select customer_id,
 count(distinct order_date) as no_of_days
 from sales
